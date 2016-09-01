6b87d5d5a3fcfceb7b457ee994847dcd78ba23f8
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2016-09-02 00:05:42 +0200

    add regex support

--- procCheck.py
+++ procCheck.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import re
 import sys
 import yaml
 import socket
@@ -10,6 +11,7 @@ import argparse
 # Functions.
 # ------------------------------------------------------------------ #
 
+#
 # Open Yaml file.
 def openYamlFile(yamlFile):
   # Check if procs list file exists.
@@ -28,7 +30,7 @@ def openYamlFile(yamlFile):
 
   return yamlFileContent
 
-
+#
 # Validate main groups names.
 def checkProcsListGroups(rowProcList, groupsList, procsListFile):
   if set(rowProcList.keys()) - set(groupsList):
@@ -36,7 +38,7 @@ def checkProcsListGroups(rowProcList, groupsList, procsListFile):
     print 'One of following groups "%s" is not found in %s' % (groupsListFormated, procsListFile)
     sys.exit(1)
 
-
+#
 # Merge all groups in one list.
 def initProcsList(rowProcList, procsListGroups, procsListFile):
   # Validate names of main groups in procs list file.
@@ -53,12 +55,17 @@ def initProcsList(rowProcList, procsListGroups, procsListFile):
 
   # Append content of groups to the list.
   for group in rowProcList:
+    if group == 'byRegex':
+      regex = True
+    else:
+      regex = False
+
     for proc, pattern in rowProcList[group].iteritems():
-      formatedProcList.append({"name": proc, "pattern": pattern})
+      formatedProcList.append({"name": proc, "pattern": pattern, "regex": regex})
 
   return formatedProcList
 
-
+#
 # Get list of procs in the system.
 def getSystemProcs():
   # Empty dict to put all procs in the system in it.
@@ -85,9 +92,14 @@ def getSystemProcs():
 
   return systemProcs
 
-
+#
 # Find procs in system procs, and print them.
-def findProcsInSystem(procsList, commonOptions):
+def printProcsInSystem(procsList, commonOptions):
+
+  def updateProcsFound(procsDict, pid, name, exe, pattern="", matchedRegex=""):
+    procData = {pid: {"name": procName, "exe": sysProcName, "pattern": procPattern, "matched_regex": matchedRegex}}
+    procsFound.update(procData)
+
   # Empty dict for all procs are found in the system.
   procsFound = dict()
   
@@ -95,28 +107,40 @@ def findProcsInSystem(procsList, commonOptions):
   for proc in procsList:
     procName = proc['name']
     procPattern = proc['pattern']
+    byRegex = proc['regex']
 
     for pid, systemProcInfo in systemProcs.iteritems():
-      # Find procs by pattern first.
-      if procPattern and procPattern in systemProcInfo["args"]:
-        procsFound.update({pid: {"name": procName, "exe": systemProcInfo["name"], "pattern": procPattern}})
-      # Find procs by bin name.
-      elif procName == systemProcInfo["name"]:
-        procsFound.update({pid: {"name": procName, "exe": systemProcInfo["name"], "pattern": ""}})
+      sysProcName = systemProcInfo["name"]
+      sysProcArgs = systemProcInfo["args"]
+
+      if byRegex:
+        regexPattern = re.compile(procPattern)
+        matchedRegex = regexPattern.findall(sysProcArgs)
+
+      # Find procs by Regex
+      if byRegex and regexPattern.search(sysProcArgs):
+        updateProcsFound(procsFound, pid, procName, sysProcName, procPattern, matchedRegex[0])
+      # Find procs by pattern (fixed string).
+      elif procPattern and procPattern in sysProcArgs:
+        updateProcsFound(procsFound, pid, procName, sysProcName, procPattern)
+      # Find procs by name.
+      elif procName == sysProcName:
+        updateProcsFound(procsFound, pid, procName, sysProcName)
   
   # Loop over procs that are found, and print them in InfluxDB format.
   for pid, procInfo in procsFound.iteritems():
     outputValues = {
-      'processName': procInfo["name"],
       'pid': pid,
       'exe': procInfo["exe"],
-      'pattern': procInfo["pattern"]
+      'pattern': procInfo["pattern"],
+      'processName': procInfo["name"],
+      'matchedRegex': procInfo['matched_regex']
     }
     outputValues.update(commonOptions)
   
     # In InfluxDB format, first group is tags names, and second group is values.
     print ('%(pluginName)s,host=%(hostname)s,process_name=%(processName)s,exe=%(exe)s,pid=%(pid)s'
-           ' host=%(hostname)s,process_name="%(processName)s",exe="%(exe)s",pid=%(pid)s,pattern="%(pattern)s"' % outputValues)
+           ' host=%(hostname)s,process_name="%(processName)s",exe="%(exe)s",pid=%(pid)s,pattern="%(pattern)s",matched_regex"%(matchedRegex)s"' % outputValues)
 
 # ------------------------------------------------------------------ #
 # Main.
@@ -154,4 +178,4 @@ if __name__ == "__main__":
   systemProcs = getSystemProcs()
 
   # Print processes that founded.
-  findProcsInSystem(procsList, mainOptions)
+  printProcsInSystem(procsList, mainOptions)
