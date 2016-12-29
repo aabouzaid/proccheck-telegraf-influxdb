dec54fba33da49bfb5df5d950e84ca75be0d0a1f
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2016-12-30 00:05:42 +0100

    tidy up the structure and workflow

--- procCheck.py
+++ procCheck.py
@@ -8,174 +8,194 @@ import socket
 import argparse
 
 # ------------------------------------------------------------------ #
-# Functions.
+# Classes/Functions.
 # ------------------------------------------------------------------ #
 
-#
-# Open Yaml file.
-def openYamlFile(yamlFile):
-  # Check if procs list file exists.
-  try:
-    os.path.isfile(yamlFile)
-  except TypeError:
-    print "Cannot open YAML file: %s." % (yamlFile)
-    sys.exit(1)
-  
-  # Load content of Yaml file.
-  with open(yamlFile, 'r') as procsYamlFile:
-    try:
-      yamlFileContent = yaml.load(procsYamlFile)
-    except yaml.YAMLError as yamlError:
-      print(yamlError)
+class script(object):
+    #
+    # Script options.
+    def arguments(self):
+        parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.add_argument("-f","--procs-list-file", default="procList.yml", help="Path for processes list in YAML file.")
+        parser.add_argument("-n","--measurement-name", default="procCheck", help="It will be used as measurement name in Telegraf.")
+        args = parser.parse_args()
+        #
+        return args
 
-  return yamlFileContent
+    #
+    # Open Yaml file.
+    def openYamlFile(self, yamlFile):
+        # Check if procs list file exists.
+        try:
+          os.path.isfile(yamlFile)
+        except TypeError:
+          print "Cannot open YAML file: %s." % (yamlFile)
+          sys.exit(1)
 
-#
-# Validate main groups names.
-def checkProcsListGroups(rowProcList, groupsList, procsListFile):
-  if set(rowProcList.keys()) - set(groupsList):
-    groupsListFormated = '", "'.join(groupsList)
-    print 'One of following groups "%s" is not found in %s' % (groupsListFormated, procsListFile)
-    sys.exit(1)
+        # Load content of Yaml file.
+        with open(yamlFile, 'r') as procsYamlFile:
+          try:
+            yamlFileContent = yaml.load(procsYamlFile)
+          except yaml.YAMLError as yamlError:
+            print(yamlError)
+        #
+        return yamlFileContent
 
-#
-# Merge all groups in one list.
-def initProcsList(rowProcList, procsListGroups, procsListFile):
-  # Validate names of main groups in procs list file.
-  checkProcsListGroups(yamlProcsList, procsListGroups, procsListFile)
+class procCheck(object):
+    #
+    # Init.
+    def __init__(self, monitoredProcsGroups):
+        self.monitoredProcsGroups = monitoredProcsGroups
 
-  # Formated list.
-  formatedProcList = list()
+    #
+    # Merge all groups in one list.
+    def initProcsList(self, yamlMonitoredProcs, monitoredProcsFile):
 
-  # Convert procs list to dict (key is proc, and value is pattern).
-  byProcName = 'byName'
-  byNameDict = dict((key,"") for key in rowProcList[byProcName])
-  del rowProcList[byProcName]
-  rowProcList[byProcName] = byNameDict
+        # Validate names of main groups in procs list file.
+        if set(yamlMonitoredProcs.keys()) - set(self.monitoredProcsGroups):
+          groupsListFormated = '", "'.join(self.monitoredProcsGroups)
+          print 'One of following groups "%s" is not found in %s' % (groupsListFormated, monitoredProcsFile)
+          sys.exit(1)
 
-  # Append content of groups to the list.
-  for group in rowProcList:
-    if group == 'byRegex':
-      regex = True
-    else:
-      regex = False
+        # Init a list of dicts, where each dict reprecents a proc.
+        formatedProcList = list()
 
-    for proc, pattern in rowProcList[group].iteritems():
-      formatedProcList.append({"name": proc, "pattern": pattern, "regex": regex})
+        # Convert procs list to dict (key is proc, and value is pattern).
+        byProcName = 'byName'
+        byNameDict = dict((key,"") for key in yamlMonitoredProcs[byProcName])
+        del yamlMonitoredProcs[byProcName]
+        yamlMonitoredProcs[byProcName] = byNameDict
 
-  return formatedProcList
+        # Append content of groups to the list.
+        for group in yamlMonitoredProcs:
+          if group == 'byRegex':
+            regex = True
+          else:
+            regex = False
 
-#
-# Get list of procs in the system.
-def getSystemProcs():
-  # Empty dict to put all procs in the system in it.
-  systemProcs = dict()
-  
-  # Loop over pids in /proc.
-  pidsList = [pid for pid in os.listdir('/proc') if pid.isdigit()]
-  
-  # Function to get info from any file inside "/proc/PID" path.
-  def getProcInfo(pid, pathName):
-    try:
-      procInfo = open(os.path.join('/proc', pid, pathName), 'rb').read().rstrip('\n').replace('\x00',' ')
-    # Handle the error in case any proc did exit while script is still working.
-    except IOError:
-      pass
-    return procInfo
-  
-  # Add proc PID, command arguments, and bin/exe name.
-  for pid in pidsList:
-    procArgs = getProcInfo(pid, 'cmdline')
-    procBin = getProcInfo(pid, 'comm')
-    if procArgs:
-      systemProcs.update({pid: {"name": procBin, "args": procArgs}})
+          for proc, pattern in yamlMonitoredProcs[group].iteritems():
+            formatedProcList.append({"name": proc, "pattern": pattern, "regex": regex})
+        #
+        return formatedProcList
 
-  return systemProcs
+    #
+    # Get list of all procs are running in the system.
+    def getSystemProcs(self):
+        # Empty dict to put all procs in the system in it.
+        systemProcs = dict()
 
-#
-# Find procs in system procs, and print them.
-def printProcsInSystem(procsList, measurementName):
+        # Loop over pids in /proc.
+        pidsList = [pid for pid in os.listdir('/proc') if pid.isdigit()]
 
-  def updateProcsFound(procsDict, pid, name, exe, pattern="", matchedRegex=""):
-    procData = {pid: {"name": procName, "exe": sysProcName, "pattern": procPattern, "matched_regex": matchedRegex}}
-    procsFound.update(procData)
+        # Function to get info from any file inside "/proc/PID" path.
+        def getProcInfo(pid, pathName):
+          try:
+            procInfo = open(os.path.join('/proc', pid, pathName), 'rb').read().rstrip('\n').replace('\x00',' ')
+          # Handle the error in case any proc did exit while script is still working.
+          except IOError:
+            pass
+          return procInfo
 
-  # Empty dict for all procs are found in the system.
-  procsFound = dict()
-  hostname = socket.gethostname()
+        # Add proc PID, command arguments, and bin/exe name.
+        for pid in pidsList:
+          procArgs = getProcInfo(pid, 'cmdline')
+          procBin = getProcInfo(pid, 'comm')
+          if procArgs:
+            systemProcs.update({pid: {"name": procBin, "args": procArgs}})
+        #
+        return systemProcs
 
-  # Find if processes are found in the system and add them to dict.
-  for proc in procsList:
-    procName = proc['name']
-    procPattern = proc['pattern']
-    byRegex = proc['regex']
+    #
+    # Update dict with proc info.
+    def updateProcsDict(self, procsDict, pid, name, exe, pattern="", matchedRegex=""):
+        procData = {pid: {"name": name, "exe": exe, "pattern": pattern, "matched_regex": matchedRegex}}
+        procsDict.update(procData)
+        #
+        return procsDict
 
-    for pid, systemProcInfo in systemProcs.iteritems():
-      sysProcName = systemProcInfo["name"]
-      sysProcArgs = systemProcInfo["args"]
+    #
+    # Find procs in system procs.
+    def findProcsInSystem(self, monitoredProcs, systemProcs):
+        # Empty dict for all procs are found in the system.
+        foundProcs = dict()
 
-      if byRegex:
-        regexPattern = re.compile(procPattern)
-        matchedRegex = regexPattern.findall(sysProcArgs)
+        # Find if processes are found in the system and add them to dict.
+        for proc in monitoredProcs:
+            procName = proc['name']
+            procPattern = proc['pattern']
+            byRegex = proc['regex']
 
-      # Find procs by Regex
-      if byRegex and regexPattern.search(sysProcArgs):
-        updateProcsFound(procsFound, pid, procName, sysProcName, procPattern, matchedRegex[0])
-      # Find procs by pattern (fixed string).
-      elif procPattern and procPattern in sysProcArgs:
-        updateProcsFound(procsFound, pid, procName, sysProcName, procPattern)
-      # Find procs by name.
-      elif procName == sysProcName:
-        updateProcsFound(procsFound, pid, procName, sysProcName)
-  
-  # Loop over procs that are found, and print them in InfluxDB format.
-  for pid, procInfo in procsFound.iteritems():
-    outputValues = {
-      'pluginName': measurementName,
-      'hostname': hostname,
-      'pid': pid,
-      'exe': procInfo["exe"],
-      'pattern': procInfo["pattern"],
-      'processName': procInfo["name"],
-      'matchedRegex': procInfo['matched_regex']
-    }
+            for pid, systemProcInfo in systemProcs.iteritems():
+                sysProcName = systemProcInfo["name"]
+                sysProcArgs = systemProcInfo["args"]
 
-    procOutputKeys = ('%(pluginName)s,host=%(hostname)s,process_name=%(processName)s,exe=%(exe)s,pid=%(pid)s' % outputValues)
-    procOutputData = ('host=%(hostname)s,process_name="%(processName)s",exe="%(exe)s",pid=%(pid)s,pattern="%(pattern)s",matched_regex"%(matchedRegex)s"' % outputValues)
+                if byRegex:
+                  regexPattern = re.compile(procPattern)
+                  matchedRegex = regexPattern.findall(sysProcArgs)
 
-    # In InfluxDB format, first group is tags names, and second group is values.
-    print procOutputKeys, procOutputData
+                # Find procs by Regex
+                if byRegex and regexPattern.search(sysProcArgs):
+                  self.updateProcsDict(foundProcs, pid, procName, sysProcName, procPattern, matchedRegex[0])
+                # Find procs by pattern (fixed string).
+                elif procPattern and procPattern in sysProcArgs:
+                  self.updateProcsDict(foundProcs, pid, procName, sysProcName, procPattern)
+                # Find procs by name.
+                elif procName == sysProcName:
+                  self.updateProcsDict(foundProcs, pid, procName, sysProcName)
+        #
+        return foundProcs
+
+    #
+    # Find procs in system procs, and print them.
+    def printProcsInSystem(self, foundProcs, measurementName):
+        hostname = socket.gethostname()
+        # Loop over procs that are found, and print them in InfluxDB format.
+        for pid, procInfo in foundProcs.iteritems():
+            outputValues = {
+              'pluginName': measurementName,
+              'hostname': hostname,
+              'pid': pid,
+              'exe': procInfo["exe"],
+              'pattern': procInfo["pattern"],
+              'processName': procInfo["name"],
+              'matchedRegex': procInfo['matched_regex']
+            }
+
+            procOutputKeys = ('%(pluginName)s,host=%(hostname)s,process_name=%(processName)s,exe=%(exe)s,pid=%(pid)s' % outputValues)
+            procOutputData = ('host=%(hostname)s,process_name="%(processName)s",exe="%(exe)s",pid=%(pid)s,pattern="%(pattern)s",matched_regex="%(matchedRegex)s"' % outputValues)
+
+            # In InfluxDB format, first group is tags names, and second group is values.
+            print procOutputKeys, procOutputData
 
 # ------------------------------------------------------------------ #
 # Main.
 # ------------------------------------------------------------------ #
 
 if __name__ == "__main__":
+    # Check if any group is missing.
+    monitoredProcsGroups = [
+      'byName',
+      'byString',
+      'byRegex'
+    ]
 
-  # Script options.
-  parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument("-f","--yml-file", default="procList.yml", help="Path for processes list in YAML file.")
-  parser.add_argument("-n","--measurement-name", default="procCheck", help="It will be used as measurement name in Telegraf.")
-  args = parser.parse_args()
-  
-  # Check if any group is missing.
-  procsListGroups = [
-    'byName',
-    'byString',
-    'byRegex'
-  ]
-  
-  # Set Yaml file path.
-  procsListFile = args.yml_file
-  
-  # Get content of Yaml file.
-  yamlProcsList = openYamlFile(procsListFile)
-  
-  # List of monitored processes.
-  procsList = initProcsList(yamlProcsList, procsListGroups, procsListFile)
+    # Arguments.
+    args = script().arguments()
+    monitoredProcsFile = args.procs_list_file
+    measurementName = args.measurement_name
 
-  # Get list with processes running now.
-  systemProcs = getSystemProcs()
+    # Get content of Yaml file.
+    yamlMonitoredProcs = script().openYamlFile(monitoredProcsFile)
 
-  # Print processes that founded.
-  printProcsInSystem(procsList, args.measurement_name)
+    # Init class.
+    pc = procCheck(monitoredProcsGroups)
+
+    # Get list of monitored processes and processes are running now.
+    monitoredProcs = pc.initProcsList(yamlMonitoredProcs, monitoredProcsFile)
+    systemProcs = pc.getSystemProcs()
+
+    # Find monitored processes are running now on the system.
+    foundProcs = pc.findProcsInSystem(monitoredProcs, systemProcs)
+
+    # Print processes that founded.
+    pc.printProcsInSystem(foundProcs, measurementName)
